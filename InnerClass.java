8374182ed410550ffc428ebf799e418baf2c4c3d
Sundar-16102003 <ssundaralingam25@gmail.com>
2026-06-06 21:48:13 +0530

    updated

--- InnerClass.java
+++ InnerClass.java
@@ -1,8 +1,8 @@
-	 class InnerClass
+	 class A
 {
 	String car="BMW";
 	
-	 class InnerClass2
+	 class B
 	{
 		 void characteristics()
 		{
@@ -12,9 +12,9 @@
 	}
 	public static void main(String[] args) 
 	{
-		InnerClass inc=new InnerClass();
+		A inc=new A();
 		System.out.println(inc.car);
-		InnerClass.InnerClass2 inc2=inc.new InnerClass2();
+		A.B inc2=inc.new B();
         inc2.characteristics();
 		//InnerClass2.characteristics();
 	//InnerClass.InnerClass2 inc2=inc.new InnerClass2();
