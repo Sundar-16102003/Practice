9217f24aa8d98507e13fd6de2ac2db1393309442
Sundar-16102003 <ssundaralingam25@gmail.com>
2026-06-10 11:44:34 +0530

    key difference between logical && and bitwise &

---
+++ Maths.java
@@ -0,0 +1,25 @@
+public class Maths {
+    public static void main(String[] args) 
+	{
+		//System.out.println(10.12<10.1);
+		int x=0;
+		
+		if(x!=0 && 10/x > 2)
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+		
+		if(x!=0 & 10/x >2)
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+	}
+}
